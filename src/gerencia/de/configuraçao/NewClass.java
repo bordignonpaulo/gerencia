388b459e9009509c92b30835231567a34e529a0f
Paulo Bordignon <paulobordignon@alunos.utfpr.edu.br>
2017-05-28 13:07:07 -0300

    Segundo Commit

---
+++ src/gerencia/de/configuraçao/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gerencia.de.configuraçao;
+
+/**
+ *
+ * @author Paulo
+ */
+public class NewClass {
+    
+}
