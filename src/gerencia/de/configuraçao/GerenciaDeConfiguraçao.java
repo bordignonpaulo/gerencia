25297c76679176d9aa65cf7a6b3ee686a2c3dd26
Paulo Bordignon <paulobordignon@alunos.utfpr.edu.br>
2017-05-28 13:02:51 -0300

    Primeiro Commit

---
+++ src/gerencia/de/configuraçao/GerenciaDeConfiguraçao.java
@@ -0,0 +1,12 @@
+
+package gerencia.de.configuraçao;
+
+
+public class GerenciaDeConfiguraçao {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
